166f71da9715713a8eb10a3c6a04cb46071b4e64
rlstallard <89097824+rlstallard@users.noreply.github.com>
2021-10-28 23:53:09 -0400

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+/*
+	Student Name: Robert Luke Stallard
+	File Name: script.js
+	Date: 10/29/2021
+*/
+
+// Mobile hamburger menu open function
+function openMobileMenu() {
+    var menu = document.getElementById("mobile-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+}
